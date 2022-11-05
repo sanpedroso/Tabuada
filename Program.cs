6c037f453267eb850931f36d0048a2ddd314d97c
= <=>
2022-11-04 21:59:20 -0300

    solucao do exercicio tabuada

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int resultado = 0;
+int valor = 0;
+Console.WriteLine("Tabuada do numero..");
+int v = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Ate qual numero deseja multiplicar:");
+int r = Convert.ToInt16(Console.ReadLine());
+while (resultado  <= r)
+{
+    valor = v * resultado;
+    Console.WriteLine(valor);
+    resultado++ ;
+}
